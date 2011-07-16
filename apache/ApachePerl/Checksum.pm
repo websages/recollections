5c927efea9ab7071a898f0e93101351898e47fd0
James S White <james@jameswhite.org>
2011-07-16 14:44:06 -0500

    working on the wd setup

--- apache/ApachePerl/Checksum.pm
+++ apache/ApachePerl/Checksum.pm
@@ -22,28 +22,29 @@ sub handler {
 
     my $c = $filter->c;
     my $r = $filter->r;
-    my $bb_ctx = APR::Brigade->new($r->pool, $r->bucket_alloc);
-    my $rv = $filter->next->get_brigade($bb, $mode, $block, $readbytes);
+print STDERR "-=[ ". ref($c) .",". ref($r) ."]=-\n";
+#    my $bb_ctx = APR::Brigade->new($r->pool, $c->bucket_alloc);
+#    my $rv = $filter->next->get_brigade($bb, $mode, $block, $readbytes);
     return $rv unless $rv == APR::Const::SUCCESS;
 
-    while (!$bb_ctx->empty) {
-        my $b = $bb_ctx->first;
-        $b->remove;
-        if ($b->is_eos) {
-            $bb->insert_tail($b);
-            last;
-        }
-        my $data;
-        my $status = $b->read($data);
-        return $status unless $status == APR::Const::SUCCESS;
-
-        if ($data and $data =~ s|^FUCK|SHIT|) {
-            my $bn = APR::Bucket->new($data);
-            $b->insert_after($bn);
-            $b->remove; # no longer needed
-            last;
-        }
-    }
+#    while (!$bb_ctx->empty) {
+#        my $b = $bb_ctx->first;
+#        $b->remove;
+#        if ($b->is_eos) {
+#            $bb->insert_tail($b);
+#            last;
+#        }
+#        my $data;
+#        my $status = $b->read($data);
+#        return $status unless $status == APR::Const::SUCCESS;
+#
+#        if ($data and $data =~ s|^FUCK|SHIT|) {
+#            my $bn = APR::Bucket->new($data);
+#            $b->insert_after($bn);
+#            $b->remove; # no longer needed
+#            last;
+#        }
+#    }
 
     return Apache2::Const::OK;
 }
