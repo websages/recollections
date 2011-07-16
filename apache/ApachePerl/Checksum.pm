ff86deddf832990eedc1c00861fdab76e9807451
James S White <james@jameswhite.org>
2011-07-16 14:51:40 -0500

    working on the wd setup

--- apache/ApachePerl/Checksum.pm
+++ apache/ApachePerl/Checksum.pm
@@ -17,34 +17,27 @@ use Apache2::Const -compile => qw(OK DECLINED);
 use APR::Brigade ( );
 use APR::Bucket ( );
 
-sub handler {
-    my($filter, $bb, $mode, $block, $readbytes) = @_;
+sub handler : FilterConnectionHandler {
+    my($f, $bb, $mode, $block, $readbytes) = @_;
 
     my $c = $filter->c;
     my $r = $filter->r;
-print STDERR "-=[ ". ref($c) .",". ref($r) ."]=-\n";
-#    my $bb_ctx = APR::Brigade->new($r->pool, $c->bucket_alloc);
+
     my $rv = $filter->next->get_brigade($bb, $mode, $block, $readbytes);
     return $rv unless $rv == APR::Const::SUCCESS;
 
-#    while (!$bb_ctx->empty) {
-#        my $b = $bb_ctx->first;
-#        $b->remove;
-#        if ($b->is_eos) {
-#            $bb->insert_tail($b);
-#            last;
-#        }
-#        my $data;
-#        my $status = $b->read($data);
-#        return $status unless $status == APR::Const::SUCCESS;
-#
-#        if ($data and $data =~ s|^FUCK|SHIT|) {
-#            my $bn = APR::Bucket->new($data);
-#            $b->insert_after($bn);
-#            $b->remove; # no longer needed
-#            last;
-#        }
-#    }
+    for (my $b = $bb->first; $b; $b = $bb->next($b)) {
+          $b->read(my $data);
+          warn("data: $data\n");
+  
+          if ($data and $data =~ s|FUCK|SHIT|) {
+              my $nb = APR::Bucket->new($bb->bucket_alloc, $data);
+              $b->insert_after($nb);
+              $b->remove; # no longer needed
+              #$f->ctx(1); # flag that that we have done the job
+              last;
+          }
+      }
 
     return Apache2::Const::OK;
 }
