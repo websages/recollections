ddcad871b98b59e186e846cbd1ab4c57cf14d79d
James S White <james@jameswhite.org>
2011-07-16 14:20:14 -0500

    working on the wd setup

--- apache/ApachePerl/Checksum.pm
+++ apache/ApachePerl/Checksum.pm
@@ -11,39 +11,38 @@ use File::Path;
 use File::Basename;
 
 # Compile constants
+use Apache2::Filter ( );
+use APR::Const     -compile => ':common SUCCESS';
 use Apache2::Const -compile => qw(OK DECLINED);
-use APR::Const     -compile => ':common';
-
-use Apache2::Connection ( );
 use APR::Brigade ( );
 use APR::Bucket ( );
 
 sub handler {
     my($filter, $bb, $mode, $block, $readbytes) = @_;
 
-    my $c = $filter->c;
-    my $bb_ctx = APR::Brigade->new($c->pool, $c->bucket_alloc);
+#    my $c = $filter->c;
+#    my $bb_ctx = APR::Brigade->new($c->pool, $c->bucket_alloc);
     my $rv = $filter->next->get_brigade($bb_ctx, $mode, $block, $readbytes);
     return $rv unless $rv == APR::Const::SUCCESS;
 
-    while (!$bb_ctx->empty) {
-        my $b = $bb_ctx->first;
-
-        $b->remove;
-
-        if ($b->is_eos) {
-            $bb->insert_tail($b);
-            last;
-        }
-
-        my $data;
-        my $status = $b->read($data);
-        return $status unless $status == APR::Const::SUCCESS;
-
-        $b = APR::Bucket->new(lc $data) if $data;
-
-        $bb->insert_tail($b);
-    }
+#    while (!$bb_ctx->empty) {
+#        my $b = $bb_ctx->first;
+#
+#        $b->remove;
+#
+#        if ($b->is_eos) {
+#            $bb->insert_tail($b);
+#            last;
+#        }
+#
+#        my $data;
+#        my $status = $b->read($data);
+#        return $status unless $status == APR::Const::SUCCESS;
+#
+#        $b = APR::Bucket->new(lc $data) if $data;
+#
+#        $bb->insert_tail($b);
+#    }
 
     return Apache2::Const::OK;
 }
