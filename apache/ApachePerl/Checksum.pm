b82a637b532f5137a127a530190842e8f1f1dd80
James S White <james@jameswhite.org>
2011-07-16 16:11:48 -0500

    working on the wd setup

--- apache/ApachePerl/Checksum.pm
+++ apache/ApachePerl/Checksum.pm
@@ -34,7 +34,7 @@ sub handler {
         $b->read(my $data);
         if($ctx->{'bytes'}){ $ctx->{'bytes'}+=length($data); }else{ $ctx->{'bytes'}=length($data); }
         warn("data: $data\n");
-        $ctx->{'sha1'}->add(chomp($data)) if $data;
+        $ctx->{'sha1'}->add_bits($data) if $data;
         print STDERR "seen_eos: ".$f->seen_eos."\n";
         print STDERR "digest: ".$ctx->{'sha1'}->hexdigest."\n";
     }
